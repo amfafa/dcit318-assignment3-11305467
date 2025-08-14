018eab0f8b2c8f1c9ff161af9f02cd805a2fca65
Jochebed Fafa kumor <fafakumor@gmail.com>
2025-08-14 18:00:13 +0000

    second

--- DCIT_financeManagementSystem/Program.cs
+++ DCIT_financeManagementSystem/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public record Transaction
+{
+    int Id;
+    DateTime Date;
+    decimal Amount;
+    string Category;
+}
+public interface ITransactionProcessor
+{
+    public void Process(Transaction transaction);
+}
+
+public class BankTransferProcessor
+{
+    void Process(Transaction transaction)
+    {
+        Console.WriteLine("Your Bank Transfer Amount  {Amount}  and Category is {Category}");
+    }
+}
+
+public class MobileMoneyProcessor
+{
+    void Process(Transaction transaction)
+    {
+        Console.WriteLine("Your Mobile money amount  {Amount}  and Category is {Category}");
+    }
+}
+public class CryptoWalletProcessor
+{
+
+}
